01dde20e33a96b63a904b756c357cac8cbe9c100
rawhide kobayashi <rawhidekobayashidess@gmail.com>
2021-01-10 17:11:27 -0600

    start DB work

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 import json
-import requests
+# import requests
+import sqlite3
 
-#import test json file and discard dead weight
+
+# import test json file
 with open('test data set.json') as json_transient:
     bp_spreadsheet = json.load(json_transient)
     del json_transient
 
-#iterate through the relevant fields in bp.tf's spreadsheet json to end up with unique item
-#information, while maintaining relevancy.  why catalog untradable objects?
-#no need to grab prices from this sheet, it updates too slowly.  this sheet merely serves
-#as a reference for items likely to be frequently traded.
+# with open('settings.json') as json_transient:
+#    settings = json.load(json_transient)
+#    del json_transient
+
+# url = 'https://backpack.tf/api/IGetPrices/v4?key=' + settings['API_Keys']['backpack.tf']
+
+# json_transient = requests.get(url)
+# bp_spreadsheet = json_transient.json()
+# del json_transient
+
+conn = sqlite3.connect('pricesheet.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS common_items (
+            defindex text PRIMARY KEY,
+            quality integer NOT NULL,
+            item_name text NOT NULL,
+            craftability text NOT NULL)''')
+
+# iterate through the relevant fields in bp.tf's spreadsheet to make a basic catalog of commonly traded items.
 for keys in bp_spreadsheet['response']['items']:
     item_name = keys
     for x in bp_spreadsheet['response']['items'][item_name]['defindex']:
-        defindex = x
-        for values in bp_spreadsheet['response']['items'][item_name]['prices']:
-            quality = values
-            for keys in bp_spreadsheet['response']['items'][item_name]['prices'][quality]['Tradable']:
-                craftability = keys
-                print(item_name, defindex, quality, craftability)
-
-                """
-                if isinstance(bp_spreadsheet['response']['items'][item_name]['prices'][quality]['Tradable'][craftability], list) == True:
+        # ignores tough break reskins and normal quality weapons present in this dataset.
+        if len(bp_spreadsheet['response']['items'][item_name]['defindex']) == 1 or (35 <= x < 15000):
+            defindex = x
+            for values in bp_spreadsheet['response']['items'][item_name]['prices']:
+                quality = values
+                # noinspection PyAssignmentToLoopOrWithParameter
+                for keys in bp_spreadsheet['response']['items'][item_name]['prices'][quality]['Tradable']:
+                    craftability = keys
                     print(item_name, defindex, quality, craftability)
-
-                do i care about catalogging the individual unusual effects of any given hat,
-                since bp classifieds can just search for all unusuals of a given hat, and steam market
-                doesn't differentiate in the first place?
-                
-                else:
-                    for keys in bp_spreadsheet['response']['items'][item_name]['prices'][quality]['Tradable'][craftability]:
-                        unusual_effect = keys
-                        print(item_name, defindex, quality, craftability, unusual_effect)
-                """
