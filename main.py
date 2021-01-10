ba04ee6b034e79a508fc6843f6800ace9f0149c3
rawhide kobayashi <rawhidekobayashidess@gmail.com>
2021-01-10 17:31:04 -0600

    creates database from bp.tf json including craftability, defindex, item base name, and quality

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import json
-# import requests
+import requests
 import sqlite3
 
 
 # import test json file
-with open('test data set.json') as json_transient:
-    bp_spreadsheet = json.load(json_transient)
-    del json_transient
-
-# with open('settings.json') as json_transient:
-#    settings = json.load(json_transient)
+# with open('test data set.json') as json_transient:
+#    bp_spreadsheet = json.load(json_transient)
 #    del json_transient
 
-# url = 'https://backpack.tf/api/IGetPrices/v4?key=' + settings['API_Keys']['backpack.tf']
+with open('settings.json') as json_transient:
+    settings = json.load(json_transient)
+    del json_transient
 
-# json_transient = requests.get(url)
-# bp_spreadsheet = json_transient.json()
-# del json_transient
+url = 'https://backpack.tf/api/IGetPrices/v4?key=' + settings['API_Keys']['backpack.tf']
+
+json_transient = requests.get(url)
+bp_spreadsheet = json_transient.json()
+del json_transient
 
 conn = sqlite3.connect('pricesheet.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS common_items (
-            defindex text PRIMARY KEY,
-            quality integer NOT NULL,
+            defindex integer PRIMARY KEY,
             item_name text NOT NULL,
+            quality integer NOT NULL,
             craftability text NOT NULL)''')
 
 # iterate through the relevant fields in bp.tf's spreadsheet to make a basic catalog of commonly traded items.
@@ -32,10 +32,15 @@ for keys in bp_spreadsheet['response']['items']:
     for x in bp_spreadsheet['response']['items'][item_name]['defindex']:
         # ignores tough break reskins and normal quality weapons present in this dataset.
         if len(bp_spreadsheet['response']['items'][item_name]['defindex']) == 1 or (35 <= x < 15000):
-            defindex = x
+            defindex = int(x)
             for values in bp_spreadsheet['response']['items'][item_name]['prices']:
                 quality = values
                 # noinspection PyAssignmentToLoopOrWithParameter
                 for keys in bp_spreadsheet['response']['items'][item_name]['prices'][quality]['Tradable']:
                     craftability = keys
                     print(item_name, defindex, quality, craftability)
+                    dbtuple = [(defindex, item_name, quality, craftability)]
+                    c.execute('INSERT OR REPLACE INTO common_items VALUES (?,?,?,?)', (defindex, item_name, quality, craftability))
+
+conn.commit()
+conn.close()
