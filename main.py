56f7c13275df5cacd2c6468fc494e831628d49f6
rawhide kobayashi <rawhidekobayashidess@gmail.com>
2021-01-11 12:14:50 -0600

    creates database from bp.tf json including craftability, defindex, item base name, and quality, with sku

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ qualities = ('Normal', 'Genuine', 'Unused', 'Vintage', 'Unused', 'Unusual', 'Uni
 
 # import test json file
 with open('test data set.json') as json_transient:
-   bp_spreadsheet = json.load(json_transient)
-   del json_transient
+    bp_spreadsheet = json.load(json_transient)
+    del json_transient
 
 with open('settings.json') as json_transient:
     settings = json.load(json_transient)
@@ -25,14 +25,14 @@ c.execute('''CREATE TABLE IF NOT EXISTS common_items (
             sku text PRIMARY KEY,
             defindex integer NOT NULL,
             item_name text NOT NULL,
-            quality integer NOT NULL,
+            quality text NOT NULL,
             craftability text NOT NULL)''')
 
 # iterate through the relevant fields in bp.tf's spreadsheet to make a basic catalog of commonly traded items.
 for keys in bp_spreadsheet['response']['items']:
     item_name = keys
     for x in bp_spreadsheet['response']['items'][item_name]['defindex']:
-        # ignores tough break reskins and normal quality weapons present in this dataset.
+        # ignores tough break reskins and default weapons present in this dataset.
         if len(bp_spreadsheet['response']['items'][item_name]['defindex']) == 1 or (35 <= x < 15000):
             defindex = x
             for values in bp_spreadsheet['response']['items'][item_name]['prices']:
@@ -40,14 +40,10 @@ for keys in bp_spreadsheet['response']['items']:
                 # noinspection PyAssignmentToLoopOrWithParameter
                 for keys in bp_spreadsheet['response']['items'][item_name]['prices'][quality]['Tradable']:
                     craftability = keys
-                    print(item_name, defindex, quality, craftability)
-                    print(int(quality))
-                    print(qualities[int(quality)])
                     if craftability == 'Craftable':
                         sku = str(defindex) + ';' + quality
                     else:
-                        sku = str(defindex) + ';' + quality + ';' + 'uncraftable'
-                    print(sku)
+                        sku = str(defindex) + ';' + quality + ';uncraftable'
                     c.execute('INSERT OR REPLACE INTO common_items VALUES (?,?,?,?,?)', (sku, defindex, item_name, qualities[int(quality)], craftability))
 
 conn.commit()
