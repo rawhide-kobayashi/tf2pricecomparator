930b11fffb3c888e730e374655d93599f9b89e6a
rawhide kobayashi <rawhidekobayashidess@gmail.com>
2021-01-13 14:50:57 -0600

    classifieds interpretation work

--- README.md
+++ README.md
@@ -4,6 +4,4 @@ A python project to gather live (approximately) pricing data from various TF2 ma
 
 Copyright © 2021 Rawhide Kobayashi rawhidekobayashidess@gmail.com
 
-This work is free. You can redistribute it and/or modify it under the 
-terms of the Do What The Fuck You Want To Public License, Version 2,
-as published by Sam Hocevar. See the COPYING file for more details.
+This program is free software. It comes without any warranty, to the extent permitted by applicable law. You can redistribute it and/or modify it under the terms of the Do What The Fuck You Want To Public License, Version 2, as published by Sam Hocevar. See the COPYING file for more details.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import sqlite3
+import time
 
 qualities = ('Normal', 'Genuine', 'Unused', 'Vintage', 'Unused', 'Unusual', 'Unique', 'Community', 'Valve', 'Self-Made', 'Unused', 'Strange', 'Unused', 'Haunted', 'Collector\'s', 'Decorated')
 
@@ -47,4 +48,23 @@ for keys in bp_spreadsheet['response']['items']:
                     c.execute('INSERT OR REPLACE INTO common_items VALUES (?,?,?,?,?)', (sku, defindex, item_name, qualities[int(quality)], craftability))
 
 conn.commit()
+
+#with open('classifieds test set.json') as json_transient:
+#    bp_classifieds = json.load(json_transient)
+#    del json_transient
+
+url = 'https://backpack.tf/api/classifieds/search/v1?item_names=0&page_size=30&item=Scattergun&quality=11&tradable=1&craftable=1&australium=-1&killstreak_tier=0&key=' + settings['API_Keys']['backpack.tf']
+
+json_transient = requests.get(url)
+bp_classifieds = json_transient.json()
+del json_transient
+
+for x in reversed(range(len(bp_classifieds['buy']['listings']))):
+    if 'automatic' not in bp_classifieds['buy']['listings'][x] or bp_classifieds['buy']['listings'][x]['bump'] < time.time() - 1800:
+        del bp_classifieds['buy']['listings'][x]
+
+for x in reversed(range(len(bp_classifieds['sell']['listings']))):
+    if 'automatic' not in bp_classifieds['sell']['listings'][x] or bp_classifieds['sell']['listings'][x]['bump'] < time.time() - 1800:
+        del bp_classifieds['sell']['listings'][x]
+
 conn.close()
